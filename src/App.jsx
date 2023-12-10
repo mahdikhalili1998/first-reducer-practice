59e5505df5823ca6e6f2f89ad233c1d640cc494d
mahdi-lhj1998 <149571502+mahdi-lhj1998@users.noreply.github.com>
2023-12-10 21:35:56 +0330

    type&payload added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,23 @@
 import { useReducer } from "react";
 const reducer = (state, action) => {
-  switch (action) {
+  switch ((action, action.type)) {
     case "increase":
       return state + 1;
     case "reset":
       return (state = 0);
     case "decrease":
       return state - 1;
-
+    case "10plus":
+      return state + action.payload;
     default:
       console.log("error darim dadash");
   }
 };
 function App() {
   const [count, dispatch] = useReducer(reducer, 0);
+  const dahHandler = () => {
+    dispatch({ type: "10plus", payload: 10 });
+  };
   const increase = () => {
     dispatch("increase");
   };
@@ -28,6 +32,7 @@ function App() {
       <p>{count}</p>
       <button onClick={increase}>increase</button>
       <button onClick={reset}>reset</button>
+      <button onClick={dahHandler}>increase + 10</button>
       <button onClick={decrease}>decrease</button>
     </>
   );
